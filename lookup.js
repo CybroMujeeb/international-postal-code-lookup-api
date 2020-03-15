1188b9d97ec39e6b2c41ae6c3ee3a906acefc460
Mujeeb Rahman <mujeeb.mx@gmx.com>
2020-03-15 14:16:55 +0530

    Arrow funtion

--- lookup.js
+++ lookup.js
@@ -3,7 +3,7 @@ function apiLookup(){
 let pincode = '695563';
 let countryCode = 'IN';
 
-$.get('https://worldpostallocations.com/'+pinCode+'/'+countryCode,(res){
+$.get('https://worldpostallocations.com/'+pinCode+'/'+countryCode,(res) =>{
 console.log(res);
 });
 
